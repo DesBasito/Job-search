96bf634f75969db53822a7809bdf4675c975a363
abu <basit.03.kg@gmail.com>
2024-03-08 16:35:29 +0600

    write a new port for my app

--- src/main/java/kg/attractor/ht49/Ht49Application.java
+++ src/main/java/kg/attractor/ht49/Ht49Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ht49Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Ht49Application.class, args);
     }
