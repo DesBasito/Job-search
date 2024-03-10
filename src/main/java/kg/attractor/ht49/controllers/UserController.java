91fbd61cf5d058c02a2617791a0997f03e58f949
abu <basit.03.kg@gmail.com>
2024-03-10 16:53:31 +0600

    added UserController

--- src/main/java/kg/attractor/ht49/controllers/UserController.java
+++ src/main/java/kg/attractor/ht49/controllers/UserController.java
@@ -1,4 +1,21 @@
 package kg.attractor.ht49.controllers;
 
+import kg.attractor.ht49.dto.UserDto;
+import kg.attractor.ht49.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService service;
+
+    @GetMapping("users")
+    public ResponseEntity<List<UserDto>> getUsers(){
+        return ResponseEntity.ok(service.getUsers());
+    }
 }
