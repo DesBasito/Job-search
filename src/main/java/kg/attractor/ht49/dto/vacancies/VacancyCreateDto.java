2fc310f726a53176578ddc06640be467fab1dd96
abu <basit.03.kg@gmail.com>
2024-04-16 01:17:48 +0600

    refcator profile page

--- src/main/java/kg/attractor/ht49/dto/vacancies/VacancyCreateDto.java
+++ src/main/java/kg/attractor/ht49/dto/vacancies/VacancyCreateDto.java
@@ -5,9 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.jetbrains.annotations.NotNull;
 
-import java.time.LocalDateTime;
 
 @Data
 @Builder
