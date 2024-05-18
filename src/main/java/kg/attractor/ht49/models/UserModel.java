c39fa5ccc11c633017c3506494b6b953b69682ba
DesBasito <116246777+DesBasito@users.noreply.github.com>
2024-05-18 15:49:19 +0600

    minor refactor

--- src/main/java/kg/attractor/ht49/models/UserModel.java
+++ src/main/java/kg/attractor/ht49/models/UserModel.java
@@ -2,15 +2,8 @@ package kg.attractor.ht49.models;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.Hibernate;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Set;
 
 
 @Getter
